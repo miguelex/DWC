a06c02d940410d382028ada3638fc91492317183
Miguel Ángel Delgado <migue@miguelandelgado.com>
2023-05-11 20:20:05 +0200

    Formato webp

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const webp = require("gulp-webp");
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo de SASS
@@ -11,9 +16,19 @@ function css(done) {
   done(); // Callback que avisa a gulp que terminamos
 }
 
+function versionWebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
 }
+
 exports.css = css; // Exportar la función para que gulp la pueda ejecutar
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
