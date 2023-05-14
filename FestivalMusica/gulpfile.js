c89ca02548b33cd5baa9d62c0694ce46caf74315
Miguel Ángel Delgado <migue@miguelandelgado.com>
2023-05-14 13:43:39 +0200

    feat (Festival de Música) finalizado proyecto

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 // Imagenes
 const cache = require("gulp-cache");
@@ -10,10 +14,16 @@ const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 
+// JavaScript
+
+const terser = require("gulp-terser-js");
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo de SASS
+    .pipe(sourcemaps.init())
     .pipe(plumber())
-    .pipe(sass()) // Compilar el archivo de SASS
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write(".")) // Compilar el archivo de SASS
     .pipe(dest("build/css")); // Guardarlo en el HDD
 
   done(); // Callback que avisa a gulp que terminamos
@@ -47,7 +57,11 @@ function versionAvif(done) {
 }
 
 function javascript(done) {
-  src("src/js/**/*.js").pipe(dest("build/js"));
+  src("src/js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/js"));
   done();
 }
 
--- FestivalMusica/src/js/app.js
+++ FestivalMusica/src/js/app.js
@@ -3,7 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function iniciarApp() {
+  navegacionFija();
   crearGaleria();
+  scrollNav();
+}
+
+function navegacionFija() {
+  const barra = document.querySelector(".header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+  const body = document.querySelector("body");
+
+  window.addEventListener("scroll", function () {
+    if (sobreFestival.getBoundingClientRect().bottom < 0) {
+      barra.classList.add("fijo");
+      body.classList.add("body-scroll");
+    } else {
+      barra.classList.remove("fijo");
+      body.classList.remove("body-scroll");
+    }
+  });
+}
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({ behavior: "smooth" });
+    });
+  });
 }
 
 function crearGaleria() {
