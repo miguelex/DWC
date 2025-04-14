988df420b8b2f1397086278d8f2fcf1dd3369ead
Miguel Ángel Delgado <migue@miguelandelgado.com>
2025-04-14 16:45:58 +0200

    Finish feature footer

--- FestivalMusicav2/gulpfile.js
+++ FestivalMusicav2/gulpfile.js
@@ -5,9 +5,9 @@ import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
 
 export function css ( done ) {
-    src('src/scss/app.scss')
+    src('src/scss/app.scss', {sourcemaps: true })
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('build/css'))
+        .pipe(dest('build/css', { sourcemaps: '.'}))
 
     done();
 }
